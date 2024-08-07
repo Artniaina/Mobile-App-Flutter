e8ac4cb4054da67f160699a96c3785c723d0908b
Kantooo <kanto.andriahariniaina@gmail.com>
2024-08-07 08:15:52 +0300

    Upload

--- lib/screens/login_page.dart
+++ lib/screens/login_page.dart
@@ -1,6 +1,6 @@
 
 
-import 'dart:convert';  // Importer pour utiliser jsonDecode
+import 'dart:convert'; 
 import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import '../models/user.dart';
